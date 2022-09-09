2baba7d09b3aa3c334951c45229d05f511873158
hernan-cosin <78047711+hernan-cosin@users.noreply.github.com>
2022-09-09 13:06:02 -0300

    added functionallity to back button

--- components/thanks-page/index.tsx
+++ components/thanks-page/index.tsx
@@ -1,14 +1,21 @@
 import { Button } from "ui/buttons";
 import { Subtitle, Title } from "ui/typography";
 import { ThanksContainer, ThanksContent } from "./styled";
+import { useRouter } from "next/router";
 
 export function ThanksPage() {
+  const router = useRouter()
+
+  function handleBackClick() {
+    router.push("/")
+  }
+
   return (
     <ThanksContainer>
       <ThanksContent>
         <Title>¡Gracias por su compra!</Title>
         <Subtitle>Hemos recibido su pago correctamente.</Subtitle>
-        <Button className="thanks-page" backgroundColor="blue">
+        <Button className="thanks-page" onClick={handleBackClick} backgroundColor="blue">
           Volver
         </Button>
       </ThanksContent>
