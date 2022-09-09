37edb50797267d58707eb2b789e2bba9d6ba076a
hernan-cosin <78047711+hernan-cosin@users.noreply.github.com>
2022-09-08 21:47:21 -0300

    fixed bug: thanks page no exported member thanksContainer

--- components/thanks-page/styled.tsx
+++ components/thanks-page/styled.tsx
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 
-export const OrdersContainer = styled.section`
+export const ThanksContainer
+ = styled.section`
   min-height: 35vh;
   padding: 50px 5px;
 `;
-export const OrdersContent = styled.div`
+export const ThanksContent = styled.div`
   max-width: fit-content;
   margin: 0 auto;
 `;
